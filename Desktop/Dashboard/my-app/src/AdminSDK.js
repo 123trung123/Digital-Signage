e05a208519d741dfb4beb18ef62cfeb78ebc58aa
TranTrung <105001804+123trung123@users.noreply.github.com>
2024-05-16 18:52:23 +0700

    Update AdminSDK.js

--- Desktop/Dashboard/my-app/src/AdminSDK.js
+++ Desktop/Dashboard/my-app/src/AdminSDK.js
@@ -10,7 +10,7 @@ const serviceAccount = require('./YOUR FILE.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://digitalsignage-21521774-default-rtdb.asia-southeast1.firebasedatabase.app' // Replace with your database URL
+  databaseURL: 'https://digitalsignage' // Replace with your database URL
 });
 
 const db = admin.firestore();
