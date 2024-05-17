154429651a15397086741e1558078cfa445b5929
TranTrung <105001804+123trung123@users.noreply.github.com>
2024-05-17 08:07:20 +0700

    Add files via upload

---
+++ Desktop/Dashboard/api/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const admin = require('firebase-admin');
+const cors = require('cors');  // Import the cors package
+const serviceAccount = require('../src/digitalsignage-21521774-firebase-adminsdk-1c4hp-2b39253bff.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const app = express();
+app.use(cors());  // Use the cors middleware
+app.use(express.json());
+
+app.post('/grantAdmin', async (req, res) => {
+  const { uid } = req.body;
+  try {
+    await admin.auth().setCustomUserClaims(uid, { admin: true });
+    res.status(200).send({ message: 'Admin privileges granted successfully' });
+  } catch (error) {
+    console.error('Error setting custom claims:', error);
+    res.status(500).send({ error: 'Error granting admin privileges' });
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
