58a2f57d3035f60808693de9c9460751fa215e7d
TranTrung <105001804+123trung123@users.noreply.github.com>
2024-05-18 16:20:32 +0700

    Add files via upload

--- Desktop/Dashboard/api/index.js
+++ Desktop/Dashboard/api/index.js
@@ -1,16 +1,30 @@
-const { json } = require('micro');
+const express = require('express');
 const admin = require('firebase-admin');
-const cors = require('micro-cors')(); 
+const cors = require('cors');  // Import the cors package
 const serviceAccount = require('./digitalsignage-21521774-firebase-adminsdk-1c4hp-2b39253bff.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-module.exports = cors((req, res) => {
-  if (req.method === 'GET') {
-    res.end('Server is running');
-  } else {
-    res.status(405).end(); 
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/" , ( req, res) => {
+  res.send("server is running");
+});
+app.post('/grantAdmin', async (req, res) => {
+  const { uid } = req.body;
+  try {
+    await admin.auth().setCustomUserClaims(uid, { admin: true });
+    res.status(200).send({ message: 'Admin privileges granted successfully' });
+  } catch (error) {
+    console.error('Error setting custom claims:', error);
+    res.status(500).send({ error: 'Error granting admin privileges' });
   }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
