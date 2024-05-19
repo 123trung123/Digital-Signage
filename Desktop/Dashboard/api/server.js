df92696ff62fbceb4808bfa068461724201b1d5e
TranTrung <105001804+123trung123@users.noreply.github.com>
2024-05-19 10:15:41 +0700

    Update server.js

--- Desktop/Dashboard/api/server.js
+++ Desktop/Dashboard/api/server.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const admin = require('firebase-admin');
 const cors = require('cors');
-const path = require('path');
-// const serviceAccount = require('./digitalsignage-21521774-firebase-adminsdk-1c4hp-2b39253bff.json');
+require('dotenv').config(); // Load environment variables from .env file
 
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-// });
-const serviceAccountPath = path.resolve(__dirname, 'digitalsignage-21521774-firebase-adminsdk-1c4hp-2b39253bff.json');
-const serviceAccount = require(serviceAccountPath);
+// Create service account object from environment variables
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
+
 const app = express();
 app.use(cors());
 app.use(express.json());
