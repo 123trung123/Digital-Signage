ec6f1c451f736f79cca0c2e8e7883046e81b0947
TranTrung <105001804+123trung123@users.noreply.github.com>
2024-05-19 09:49:47 +0700

    Update server.js

--- Desktop/Dashboard/api/server.js
+++ Desktop/Dashboard/api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const admin = require('firebase-admin');
 const cors = require('cors');
-const serviceAccount = require('./digitalsignage-21521774-firebase-adminsdk-1c4hp-2b39253bff.json');
+// const serviceAccount = require('./digitalsignage-21521774-firebase-adminsdk-1c4hp-2b39253bff.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
