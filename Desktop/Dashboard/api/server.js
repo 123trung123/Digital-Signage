98a638b5bbb6eb2e6b865b22464cf11753cd8a69
TranTrung <105001804+123trung123@users.noreply.github.com>
2024-05-19 10:39:09 +0700

    Update server.js

--- Desktop/Dashboard/api/server.js
+++ Desktop/Dashboard/api/server.js
@@ -14,7 +14,7 @@ const serviceAccount = {
   type: process.env.FIREBASE_TYPE,
   project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: `-----BEGIN PRIVATE KEY-----\n${process.env.FIREBASE_PRIVATE_KEY}\n-----END PRIVATE KEY-----\n`,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: process.env.FIREBASE_AUTH_URI,
