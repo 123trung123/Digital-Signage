2067ec150e0d01593f4d9998a36c4ab8808b0bc9
TranTrung <105001804+123trung123@users.noreply.github.com>
2024-05-29 07:52:08 +0700

    Add files via upload

---
+++ Desktop/Dashboard/my-app/src/App.jsx
@@ -0,0 +1,229 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route, Link, NavLink } from 'react-router-dom';
+
+// Components
+import Signin from './components/auth/Signin';
+import Logs from './pages/Home/Logs';
+import Upload from './pages/Assets/Upload';
+import AccountManagement from './pages/Admin-Management/AccountManagement';
+import PlayerManagement from './pages/Player-Management/PlayerManagement';
+import PlayerHome from './pages/Player-Management/PlayerHome';
+import AdminHome from './pages/Admin-Management/AdminHome';
+
+// Firebase configuration and initialization
+import firebaseConfig from './firebaseconfig-key/firebaseConfig';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref as dbRef, onValue, onDisconnect, set as setDB } from 'firebase/database';
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
+import { ref as storageRef, uploadBytes, getDownloadURL, listAll } from 'firebase/storage';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+import { v4 } from 'uuid';
+
+import { storage } from './firebaseconfig-key/firebaseConfig';
+import { auth } from './firebaseconfig-key/firebaseConfig';
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const firestore = getFirestore(app);
+const setdatabase = dbRef(database, 'PIN');
+const pinsCollection = collection(firestore, 'pins');
+
+
+const App = () => {
+  const [imageUpload, setImageUpload] = useState(null);
+  const [pins, setPins] = useState([]);
+  const [imageUrls, setImageUrls] = useState([]);
+  const [user, setUser] = useState(null);
+  const [isOnline, setIsOnline] = useState(true);
+  const [otherMachineStatus, setOtherMachineStatus] = useState({});
+  const [initialLoad, setInitialLoad] = useState(true); // New flag
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const handleLogout = () => {
+    signOut(auth)
+      .then(() => {
+        setUser(null);
+      })
+      .catch((error) => {
+        console.error('Error signing out: ', error);
+      });
+  };
+  useEffect(() => {
+    const unsubscribeRealtimeDB = onValue(setdatabase, (snapshot) => {
+      let pinArray = Object.values(snapshot.val() || {});
+      setPins(pinArray);
+    });
+
+    return () => {
+      unsubscribeRealtimeDB();
+    };
+  }, []);
+
+  useEffect(() => {
+    const imagesListRef = storageRef(storage, 'images/');
+
+    listAll(imagesListRef).then((response) => {
+      const urls = response.items.map((item) => getDownloadURL(item));
+
+      Promise.all(urls).then((downloadUrls) => {
+        setImageUrls(downloadUrls);
+      });
+    });
+  }, []);
+
+  const toggleDarkMode = () => {
+    const newDarkMode = !isDarkMode;
+    setIsDarkMode(newDarkMode);
+    localStorage.setItem('darkMode', newDarkMode ? 'enabled' : 'disabled');
+  };
+  useEffect(() => {
+    const darkModePreference = localStorage.getItem('darkMode');
+    if (darkModePreference === 'enabled') {
+      setIsDarkMode(true);
+    } else {
+      setIsDarkMode(false);
+    }
+  }, []);
+
+  const uploadFile = () => {
+  
+    if (imageUpload == null) return;
+  
+    const fileType = imageUpload.type.startsWith('image/') ? 'image' : 'video';
+  
+    const fileId = v4();
+    const fileRef = storageRef(storage, `${fileType}s/${imageUpload.name + fileId}`);
+  
+    uploadBytes(fileRef, imageUpload)
+      .then((snapshot) => {
+        getDownloadURL(snapshot.ref)
+          .then((url) => {
+            addDoc(pinsCollection, { 
+              imageUrl: url, 
+              userId: "Store",
+              type: fileType, 
+              isActive: true,
+              Id: fileId,
+              playMode: "alwaysOn",
+              activationTime: null, 
+              endTime: null 
+            })
+            .then(() => {
+              setImageUrls((prevUrls) => [...prevUrls, url]);
+            })
+            .catch((error) => {
+              console.error('Error adding document: ', error);
+            });
+          })
+          .catch((error) => {
+            console.error('Error getting download URL: ', error);
+          });
+      })
+      .catch((error) => {
+        console.error('Error uploading file: ', error);
+      });
+  };
+  
+  useEffect(() => {
+    const setUserStatus = () => {
+      if (!user) return;
+
+      const presenceRef = dbRef(database, `.info/connected`);
+
+      onValue(presenceRef, (snapshot) => {
+        if (snapshot.val() === false) {
+          setIsOnline(false);
+          return;
+        }
+
+        setIsOnline(true);
+
+        const userStatusRef = dbRef(database, `/status/${user.uid}`);
+
+        const isOfflineForDatabase = { state: 'offline', last_changed: new Date().toString() };
+        const isOnlineForDatabase = { state: 'online', last_changed: new Date().toString() };
+        
+        setDB(userStatusRef, isOnlineForDatabase);
+        
+        onDisconnect(userStatusRef).set(isOfflineForDatabase);
+      });
+    };
+    
+    setUserStatus();
+    
+    const unsubscribeOtherMachines = onValue(dbRef(database, '/status'), (snapshot) => {
+      const statusData = snapshot.val();
+      setOtherMachineStatus(statusData || {});
+    });
+
+    const unsubscribeAuth = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setInitialLoad(false); 
+    });
+    return () => {
+      unsubscribeOtherMachines();
+      unsubscribeAuth();
+    };
+  }, [user, initialLoad]); 
+  return (
+    <BrowserRouter>
+    <div className={isDarkMode ? 'app-container dark-mode' : 'app-container'}>
+    <div className="upper_bar">
+    
+    <span className="top-bar-title"><img src="../icon.png" alt="Icon" className="title-icon" /> DSS Player</span>
+        <button onClick={toggleDarkMode} className="navToggleButton">
+          <img src={isDarkMode ? "../Sun.png" : "../Moon.png"}  className="toggle-icon" />
+        </button>
+          
+          {user && (
+            <div className="logoutContainer">
+              <button onClick={handleLogout} className="Delete">Logout</button>
+            </div>
+          )}
+        </div>
+
+          <div className="nav">
+            <div className="navButtons">
+              {!user && (
+                <>
+                  <Link to="/signin" className="navButton">Sign In</Link>
+                </>
+              )}
+              {user && (
+                <>
+                  {/* <Link to="/" className="navButton">Home</Link> */}
+                  <p className="nav-heading">GENERAL</p>
+                  <NavLink  to="/Home" className="navButton">Dashboard</NavLink>
+                  <p className="nav-heading">MANAGEMENT</p>
+                  <NavLink  to="/Assets" className="navButton">Asset</NavLink>
+                  <NavLink  to="/player" className="navButton">Player</NavLink>
+                  <NavLink  to="/account-management" className="navButton">Admin</NavLink>
+                </>
+              )}
+            </div>
+          </div>
+        <div className="content">
+          <Routes>
+            <Route path="/signin" element={<Signin setUser={setUser} />} />
+            {/* <Route path="/" element={<Home user={user} isOnline={isOnline} otherMachineStatus={otherMachineStatus} pins={pins} imageUrls={imageUrls} />} /> */}
+            <Route path="/Home" element={<Logs pins={pins} imageUrls={imageUrls} imageUpload={imageUpload} otherMachineStatus={otherMachineStatus} setImageUpload={setImageUpload} uploadFile={uploadFile} />} />
+            <Route path="/Assets" element={<Upload pins={pins} imageUrls={imageUrls} imageUpload={imageUpload} otherMachineStatus={otherMachineStatus} setImageUpload={setImageUpload} uploadFile={uploadFile} uuid={v4()} />} />
+            <Route path="/account-management/*" element={
+              <>
+                <AdminHome user={user} isOnline={isOnline} otherMachineStatus={otherMachineStatus} pins={pins} imageUrls={imageUrls} />
+                <AccountManagement />
+              </>
+            } />
+            <Route path="/player/*" element={
+              <>
+                <PlayerHome user={user} isOnline={isOnline} otherMachineStatus={otherMachineStatus} pins={pins} imageUrls={imageUrls} />
+                <PlayerManagement />
+              </>
+            } />
+          </Routes> 
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ Desktop/Dashboard/my-app/src/pages/Assets/Upload.js
@@ -0,0 +1,122 @@
+import React, { useState, useEffect } from 'react';
+import { collection, onSnapshot, getFirestore, doc, deleteDoc } from 'firebase/firestore';
+import { storage } from '../../firebaseconfig-key/firebaseConfig';
+import { ref, deleteObject } from 'firebase/storage';
+import '../../components/Style/Assets.css';
+const firestore = getFirestore();
+
+const Upload = ({ imageUpload, setImageUpload, uploadFile, uuid,type }) => {
+  const [previewUrl, setPreviewUrl] = useState(null);
+  const [isUploading, setIsUploading] = useState(false);
+  const [isActive, setIsActive] = useState(true); 
+  const [imageUrls, setImageUrls] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(firestore, 'pins'), (snapshot) => {
+      const urls = snapshot.docs.map(doc => ({ id: doc.id, url: doc.data().imageUrl, type:doc.data().type }));
+      setImageUrls(urls);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const handleFileUpload = async () => {
+    if (!imageUpload) return;
+    setIsUploading(true);
+
+    try {
+      await uploadFile();
+      setPreviewUrl(null);
+      setIsActive(true);
+    } catch (error) {
+      console.error('Error uploading file:', error);
+    } finally {
+      setIsUploading(false);
+    }
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setImageUpload(file);
+
+    const reader = new FileReader();
+    reader.onload = (e) => setPreviewUrl(e.target.result);
+    reader.readAsDataURL(file);
+  };
+
+  const toggleActivation = () => {
+    setIsActive(!isActive); 
+  };
+
+  const handleDelete = async (imageUrl, imageId, uuid) => {
+    try {
+      await deleteDoc(doc(firestore, 'pins', imageId));
+  
+      // const fileName = imageUrl.split('/').pop().split('?')[0];
+  
+      // const fileIdWithUUID = `${fileName}-${uuid}`;
+  
+      // const imageRef = ref(storage, `images/${fileIdWithUUID}`);
+
+const decodedUrl = decodeURIComponent(imageUrl);
+
+const fileName = decodedUrl.split('/').pop().split('?')[0];
+
+const fileIdWithUUID = `${fileName}`;
+
+  	const imageRef = ref(storage, `images/${fileIdWithUUID}`);
+      await deleteObject(imageRef).then(() => {
+        console.log('File deleted successfully');
+      }).catch((error) => {
+        console.error('Error deleting image:', error);
+      });
+    const videoRef = ref(storage, `videos/${fileIdWithUUID}`);
+    await deleteObject(videoRef).then(() => {
+      console.log('Video file deleted successfully');
+    }).catch((error) => {
+      console.error('Error deleting video file:', error);
+    });
+    } catch (error) {
+      console.error('Error deleting image:', error);
+    }
+  };
+  
+  return (
+    <div>
+      <div className="gallery">
+        <h2>Gallery</h2>
+        <div className="image-list">
+          {imageUrls.map(({ id, url, type }) => (
+          <div key={id} className="image-item">
+            {type === "image" ? (
+              <img src={url} alt="Uploaded" />
+            ) : (
+              <video src={url} controls />
+            )}
+            <button onClick={() => handleDelete(url, id, uuid)}>Delete</button>
+          </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="upload-container">
+        <h2>Upload</h2>
+        <div className="upload-field">
+          <input type="file" onChange={handleFileChange} />
+          {previewUrl && <img src={previewUrl} alt="Uploaded" />}
+        </div>
+        <div className="activation-toggle">
+          <label htmlFor="activationToggle">Active:</label>
+          <input type="checkbox" id="activationToggle" checked={isActive} onChange={toggleActivation} />
+        </div>
+        <button className="upload-button" onClick={handleFileUpload} disabled={isUploading}>
+          {isUploading ? 'Uploading...' : 'Upload Asset'}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Upload;
